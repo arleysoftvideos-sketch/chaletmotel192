1a50c00db89454d35c3cfa0af40dbbeafd6b799f
Arly-ia <Arlyia2024@gmail.com>
2026-05-28 23:33:36 -0400

    Change First month free to 1 month free

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,7 +149,7 @@
                                     <svg class="w-2 h-2 fill-current" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
                                     <svg class="w-2 h-2 fill-current" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
                                 </div>
-                                <span class="block text-[13px] font-black font-outfit leading-tight uppercase text-navy">{{ __('First') }}</span>
+                                <span class="block text-[24px] font-black font-outfit leading-none uppercase text-navy mb-1">1</span>
                                 <span class="block text-[18px] font-black font-outfit leading-none uppercase text-navy">{{ __('Month') }}</span>
                                 <span class="block text-[18px] font-black font-outfit leading-none uppercase text-navy">{{ __('Free!') }}</span>
                                 <div class="flex gap-0.5 text-navy mt-0.5">
