da3e2ede1c88c650e64141a07eb542ef2c403967
Arly-ia <Arlyia2024@gmail.com>
2026-08-20 23:29:17 -0400

    feat: updated LiquorGuard Android APK with new launcher icons, auto camera permissions and username prefix login support

--- android/app/src/main/java/com/example/chaletrooms/MainActivity.java
+++ android/app/src/main/java/com/example/chaletrooms/MainActivity.java
@@ -1,5 +1,44 @@
 package com.example.chaletrooms;
 
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (this.getBridge() != null && this.getBridge().getWebView() != null) {
+            WebView webView = this.getBridge().getWebView();
+            WebSettings settings = webView.getSettings();
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setDatabaseEnabled(true);
+            settings.setMediaPlaybackRequiresUserGesture(false);
+            settings.setAllowFileAccess(true);
+            settings.setAllowContentAccess(true);
+
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            cookieManager.setAcceptThirdPartyCookies(webView, true);
+
+            webView.setWebChromeClient(new WebChromeClient() {
+                @Override
+                public void onPermissionRequest(final PermissionRequest request) {
+                    runOnUiThread(() -> {
+                        request.grant(request.getResources());
+                    });
+                }
+            });
+        }
+    }
+}
